bbae522f4dbd076e2900377fe0bf7320c2f0fe02
28328-png <28328@blm.ac.th>
2025-12-04 11:03:31 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    basic.showNumber(0)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showIcon(IconNames.Sword)
+    basic.showString("นาย ชลิตชัย ภูปาทา และ นาย ธนาคาร ชัยศิริ  บางละมุงschool ")
+    basic.pause(100)
+})
